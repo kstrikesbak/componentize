1cc77901c3799f8e2d09d8a380c535b602763363
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-11-26 00:52:38 -0500

    css added and refresh added

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ document.querySelector('.add-todo').addEventListener('click', function(){
     newLi = document.createElement('li')
     result = document.querySelector('.todo-input').value
     document.querySelector('.todo-list').appendChild(newLi).innerText = result
-    })
+    refresh()
+})
+
+const refresh = function () {
+    document.querySelector('.todo-input').value = ''
+}
+
+
 
     // document.querySelector('.add-todo').addEventListener('click', function(){
     //     document.querySelector('.todo-list').appendChild(document.createElement('li')).innerText = document.querySelector('.todo-input').value
@@ -19,5 +26,16 @@ document.querySelector('.remove-todo').addEventListener('click', function(){
     document.querySelector(`.todo-list li:nth-child(${document.querySelector('.index-input').value}`).remove();
 })
 
-let list = Array.from(document.querySelectorAll('ul li'))
+// let list = Array.from(document.querySelectorAll('li'))
+
+// document.querySelector('.remove-todo').addEventListener('click', function(){
+
+//     const listWithLis = document.querySelector('#todo-list')
+    
+//     while (listWithLis.hasChildNodes()){
+
+//         listWithLis.removeChild(listWithLis.firstChild);
+
+//     }
+// })
 
